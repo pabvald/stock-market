036515837a9583a459f7d77773da8535c69267dd
pabvald <valdunciel98@gmail.com>
2019-03-09 14:30:13 +0100

    Pestaña mercados: llayout, empresas y operar.

--- angular/src/app/components/markets/index.ts
+++ angular/src/app/components/markets/index.ts
@@ -1,9 +1,121 @@
 import { Component } from "@angular/core";
 
+
+interface Company{
+    name: string,
+    code: string,
+    price: number,
+}
+
 @Component({
     selector: "market-page",
-    templateUrl: "market.html"
+    templateUrl: "market.html",
+    styleUrls:["market.css"]
 })
 export class MarketComponent{
+
+    companies : Company[];
+    selectedCompany : Company;
+   
+    constructor() {
+        this.companies = [ {
+              name: "Google",
+              code: "GOOG",
+              price: 45
+          },{
+              name: "Microsoft",
+              code: "MSFT",
+              price: 23
+          },{
+              name : "Apple",
+              code : "AAPL",
+              price : 34
+          },{
+              name : "Marvell",
+              code : "MRVL",
+              price : 13
+          },{
+              name : "Cisco Systems",
+              code : "CSCO",
+              price : 65
+          }, {
+              name : "Intel",
+              code : "INTC",
+              price : 32
+          }, {
+              name : "Ebay Inc.",
+              code : "EBAY",
+              price : 14
+          }, {
+            name: "Google",
+            code: "GOOG",
+            price: 45
+        },{
+            name: "Microsoft",
+            code: "MSFT",
+            price: 23
+        },{
+            name : "Apple",
+            code : "AAPL",
+            price : 34
+        },{
+            name : "Marvell",
+            code : "MRVL",
+            price : 13
+        },{
+            name : "Cisco Systems",
+            code : "CSCO",
+            price : 65
+        }, {
+            name : "Intel",
+            code : "INTC",
+            price : 32
+        }, {
+            name : "Ebay Inc.",
+            code : "EBAY",
+            price : 14
+        }, {
+          name: "Google",
+          code: "GOOG",
+          price: 45
+      },{
+          name: "Microsoft",
+          code: "MSFT",
+          price: 23
+      },{
+          name : "Apple",
+          code : "AAPL",
+          price : 34
+      },{
+          name : "Marvell",
+          code : "MRVL",
+          price : 13
+      },{
+          name : "Cisco Systems",
+          code : "CSCO",
+          price : 65
+      }, {
+          name : "Intel",
+          code : "INTC",
+          price : 32
+      }, {
+          name : "Ebay Inc.",
+          code : "EBAY",
+          price : 14
+      }
+          ];
+    }
+
+    /**
+     * Sets the selectedCompany.
+     * @param company clicked company
+     */
+    onSelect(company : Company) {
+        this.selectedCompany = company;
+    }
+
+    ngOnInit() {
+        this.selectedCompany = this.companies[0];
+    }
 
 }
