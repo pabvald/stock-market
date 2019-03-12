59098cb5c121b7ae295dc5b3f80998d8dafc1dee
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2019-03-12 18:01:48 +0100

    Fix Formación

--- angular/src/app/components/training/training.component.ts
+++ angular/src/app/components/training/training.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css']
 })
-export class TrainingComponent implements OnInit {
+export class TrainingComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
