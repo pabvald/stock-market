a308ee5562685670b64b9962f447e5652479685c
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2019-03-26 13:45:49 +0100

    Fix Angular routes

--- nodejs/src/app.ts
+++ nodejs/src/app.ts
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/../../angular/dist"));
 
 app.get("/*",function(req,res){
-	res.sendFile("index.html",{root: __dirname});
+	res.sendFile("index.html",{root: __dirname + "/../../angular/dist"});
 });
 
 
