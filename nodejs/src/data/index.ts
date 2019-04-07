72c0de12878052cf4f73028f7e4a28bd9783752f
pabvald <valdunciel98@gmail.com>
2019-04-07 22:43:01 +0200

    Solucionado problemas de permisos de postgresql

--- nodejs/src/data/index.ts
+++ nodejs/src/data/index.ts
@@ -9,22 +9,27 @@ async function update(){
     const query_current = `
     SELECT DISTINCT P.empresa,
         (SELECT P2.precio 
-        FROM precioAccion P2 
+        FROM precioaccion P2 
         WHERE 
             P2.empresa=P.empresa 
         ORDER BY fecha DESC 
         LIMIT 1)
-    FROM precioAccion P;
+    FROM precioaccion P;
     `;
-    let current_data = await db.query(query_current);
-    let normal = random.normal(0,500);
-    for(let row of current_data.rows){
-        let precio: number = row.precio;
-        precio += Math.round(normal());
-        precio = Math.max(1,precio);
-        let res = await db.query("INSERT INTO precioAccion VALUES (DEFAULT,$1,NOW(),$2)",[row.empresa,precio]);
+    try { 
+        let current_data = await db.query(query_current);
+        let normal = random.normal(0,500);
+        for(let row of current_data.rows){
+            let precio: number = row.precio;
+            precio += Math.round(normal());
+            precio = Math.max(1,precio);
+            let res = await db.query("INSERT INTO precioAccion(id, empresa, fecha, precio) VALUES (DEFAULT,$1,NOW(),$2)",[row.empresa,precio]);
+        }
+        console.log("END UPDATING TASK");
+    } catch (err) {
+        console.log("ERROR in UPDATE!!");
+        console.log(err.stack);
     }
-    console.log("END UPDATING TASK");
 }
 update();
 setInterval(update,1000*60); // Cada minuto
